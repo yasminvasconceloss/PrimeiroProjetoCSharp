83b9c383bedcef7abbbcc103aa1f55b0c11a178f
Yasmin Vasconcelos <yasminvasconcelos59@gmail.com>
2022-07-05 16:59:08 -0300

    Adicionei os métodos e alguns comentários da aula

--- MeuPrimeiroProjeto/ContaCorrente.cs
+++ MeuPrimeiroProjeto/ContaCorrente.cs
@@ -1,5 +1,5 @@
-public class ContaCorrente     //classe pública 
-{
+public class ContaCorrente     //classe pública      
+{ 
     public string Titular;      //para criar variável, colocar seu tipo
     public int Agencia;
     public int Numero;
@@ -8,7 +8,33 @@ public class ContaCorrente     //classe pública
     public ContaCorrente (string Titular, int Agencia, int Numero, double Saldo){
         this.Titular = Titular;
         this.Agencia = Agencia;
-        this.Numero = Numero;
+        this.Numero = Numero;  
         this.Saldo = Saldo;
     }
+    public bool Sacar(double valorSaque) {  //Método: SACAR do tipo booleano (true or false)
+        if (this.Saldo >= valorSaque){
+            this.Saldo = this.Saldo - valorSaque;
+            return true;
+        }
+        return false;
+    }
 }
+
+
+// int, string, double, float são "atributos"!
+
+// CLasse seria a "forma" do bolo.
+
+// C# oferece a possibilidade de termos classes públicas e privadas.
+    //Classes Públicas (public class): 
+    //Classes Privadas (private class): 
+
+// nome da classe, atributo embaixo
+
+//classe= modificador de acesso, class, nome
+
+//construtor = classe, terá um método escrito que receberá o mesmo nome que a classe. Não tem retorno
+
+//atributo = modificador de acesso, tipo e nome do atributo
+
+//Método = dentro da classe, modificador de acesso. SEMPRE UMA AÇÃO
--- MeuPrimeiroProjeto/Program.cs
+++ MeuPrimeiroProjeto/Program.cs
@@ -10,15 +10,32 @@ namespace PrimeiroProjeto
             ContaCorrente conta_andre = new ContaCorrente ("andre", 12345, 587, 187);    // variável do tipo conta corrente 
             ContaCorrente conta_vitor = new ContaCorrente ("Vitor", 12345, 741, 963);    // variável do tipo conta corrente 
 
-           
+            
             Console.WriteLine("A conta é do (a) " + conta_yasmin.Titular + 
-            ", a agência é " + conta_yasmin.Agencia + " e o número é " + conta_yasmin.Numero + ".");
+            ", a agência é " + conta_yasmin.Agencia + " e o número é " + conta_yasmin.Numero + " Saldo: " + conta_yasmin.Saldo);
 
             Console.WriteLine("A conta é do (a) " + conta_andre.Titular + 
-            ", a agência é " + conta_andre.Agencia + " e o número é " + conta_andre.Numero + ".");
+            ", a agência é " + conta_andre.Agencia + " e o número é " + conta_andre.Numero + " Saldo: " + conta_andre.Saldo);
 
             Console.WriteLine("A conta é do (a) " + conta_vitor.Titular + 
-            ", a agência é " + conta_vitor.Agencia + " e o número é " + conta_vitor.Numero + ".");
+            ", a agência é " + conta_vitor.Agencia + " e o número é " + conta_vitor.Numero + " Saldo: " + conta_vitor.Saldo);
+
+
+            bool sacar_conta = conta_yasmin.Sacar(100);
+            bool sacar_conta1 = conta_andre.Sacar(100);
+            bool sacar_conta2 = conta_vitor.Sacar(100);
+
+
+             Console.WriteLine("A conta é do (a) " + conta_yasmin.Titular + 
+            ", a agência é " + conta_yasmin.Agencia + " e o número é " + conta_yasmin.Numero + " Saldo: " + conta_yasmin.Saldo);
+
+            Console.WriteLine("A conta é do (a) " + conta_andre.Titular + 
+            ", a agência é " + conta_andre.Agencia + " e o número é " + conta_andre.Numero + " Saldo: " + conta_andre.Saldo);
+
+             Console.WriteLine("A conta é do (a) " + conta_vitor.Titular + 
+            ", a agência é " + conta_vitor.Agencia + " e o número é " + conta_vitor.Numero + " Saldo: " + conta_vitor.Saldo);
+
+           
 
         }
     }    
